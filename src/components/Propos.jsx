f98195651dda02267aaa1792d162b3095f9a3c0c
Thomas Lopes <thomaslopes001@gmail.com>
2023-09-19 15:18:47 +0200

    Ajout d'éléments textes(h2,p) au component Propos

--- src/components/Propos.jsx
+++ src/components/Propos.jsx
@@ -1,6 +1,14 @@
+import "../assets/styles/Propos.css"
+
+
 function Propos() {
     return (
-      <div></div>
+        <section id="Propos" className="propos">
+            <h2>A propos de moi</h2>
+            <p>Passionné par les technologies et le développement web, je suis en train de finaliser ma formation en tant que développeur web front-end.
+                Mon parcours professionnel m'a conduit à une reconversion vers cette passion qui m'anime au quotidien.</p>
+            
+        </section>
     );
   }
   
