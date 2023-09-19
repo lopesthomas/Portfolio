082a9786bb6f2debb7b1c6d8786e25fd174de9ab
Thomas Lopes <thomaslopes001@gmail.com>
2023-09-19 15:21:37 +0200

    Ajout d'un formulaire de Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,20 @@
+import "../assets/styles/Contact.css"
+
 function Contact() {
     return (
-      <div></div>
+        <section className="contact">
+            <div className="box">
+                <h2 id="contacter">Contact</h2>
+                <form action="mailto:emailid@example.com" method="post" encType="text/plain">
+                    <label value="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="Entrer votre email (ex: jean@gmail.com)" />
+                    <label value="message">Message</label>
+                    {/* <input type="text" name="" id="message" placeholder="Que souhaitez-vous dire ?" /> */}
+                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Que souhaitez-vous dire ?"></textarea>
+                    <input type="submit" value="envoyer" />
+                </form>
+            </div>
+        </section>
     );
   }
   
