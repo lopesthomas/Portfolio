d3d9f52b5f165779fe6ac03df7dc15e5072e9713
Thomas Lopes <thomaslopes001@gmail.com>
2023-09-19 15:14:02 +0200

    Ajout éléments images et titre au component Techno

--- src/components/Techno.jsx
+++ src/components/Techno.jsx
@@ -1,6 +1,24 @@
+import "../assets/styles/Techno.css"
+
 function Techno() {
     return (
-      <div></div>
+        <section id="Techno" className="technologies">
+            <h2>Technologies</h2>
+            <div className="tech-icons">
+                <img src="https://cdn-icons-png.flaticon.com/128/5968/5968267.png" data-src="https://cdn-icons-png.flaticon.com/128/5968/5968267.png" alt="Html 5 " title="Html 5 " width="64" height="64" srcSet="https://cdn-icons-png.flaticon.com/128/5968/5968267.png 4x"/>
+                <img src="https://cdn-icons-png.flaticon.com/128/5968/5968242.png" data-src="https://cdn-icons-png.flaticon.com/128/5968/5968242.png" alt="Css 3 " title="Css 3 " width="64" height="64"  srcSet="https://cdn-icons-png.flaticon.com/128/5968/5968242.png 4x"></img>
+                <img src="https://cdn-icons-png.flaticon.com/128/5968/5968292.png" data-src="https://cdn-icons-png.flaticon.com/128/5968/5968292.png" alt="Js " title="Js " width="64" height="64"  srcSet="https://cdn-icons-png.flaticon.com/128/5968/5968292.png 4x"/>
+                <img src="https://cdn-icons-png.flaticon.com/128/3334/3334886.png" data-src="https://cdn-icons-png.flaticon.com/128/3334/3334886.png" alt="Structure " title="Structure " width="64" height="64" srcSet="https://cdn-icons-png.flaticon.com/128/3334/3334886.png 4x"></img>
+                <img src="https://cdn-icons-png.flaticon.com/128/9423/9423278.png" data-src="https://cdn-icons-png.flaticon.com/128/9423/9423278.png" alt="Json " title="Json " width="64" height="64" srcSet="https://cdn-icons-png.flaticon.com/128/9423/9423278.png 4x"></img>
+                <img srcSet="https://img.icons8.com/?size=2x&amp;id=kg46nzoJrmTR&amp;format=png 2x, https://img.icons8.com/?size=1x&amp;id=kg46nzoJrmTR&amp;format=png 1x" src="https://img.icons8.com/?size=2x&amp;id=kg46nzoJrmTR&amp;format=png 2x" alt="Express Js icon" width="64" height="64" ></img>
+                <img srcSet="https://img.icons8.com/?size=256&id=54087&format=png 2x, https://img.icons8.com/?size=256&id=54087&format=png 1x" src="https://img.icons8.com/?size=256&id=54087&format=png 2x" alt="color" loading="lazy" width="64" height="64" lazy="loaded"></img>
+                <img srcSet="https://img.icons8.com/?size=256&id=74402&format=png 2x, https://img.icons8.com/?size=256&id=74402&format=png 1x" src="https://img.icons8.com/?size=256&id=74402&format=png 2x" alt="MongoDB icon" width="64" height="64"/>
+
+                
+            </div>
+
+        
+        </section>
     );
   }
   
