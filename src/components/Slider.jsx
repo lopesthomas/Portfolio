359b9cf576913a51035da51d6f1ea736cf30368e
Thomas Lopes <thomaslopes001@gmail.com>
2023-09-19 14:56:15 +0200

    Creation de Modal.css

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,3 +1,12 @@
+import React, { useState } from 'react';
+import PureModal from 'react-pure-modal';
+import '../assets/styles/Modal.css'
+import "../assets/styles/Slider.css"
+
+
+
+
+
 function Slider() {
     return (
       <div></div>
